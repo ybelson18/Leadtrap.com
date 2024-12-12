9eebbdcdfdf7ff4c2736361cbd61d7d31b9b655b
Yoni Belson <ybelson18@gmail.com>
2024-12-13 01:23:59 +0200

    fix: simplify hero subheading text

--- components/hero.tsx
+++ components/hero.tsx
@@ -130,7 +130,7 @@ export const Hero = () => {
             data-br=":R4l7puja:"
             data-brr="1"
           >
-            Use AI to push an endless stream of premium, ready-to-close leads from your website straight to your inbox, phone, or Slack.
+            Use AI to push an endless stream of premium, ready-to-close leads straight from your website to your inbox, phone, or Slack.
           </span>
         </Subheading>
         <div className="flex items-center gap-4 justify-center my-10 relative z-10">
