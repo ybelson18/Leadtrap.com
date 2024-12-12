25ac6357bc2356b6f6083fdf628e6f3f4dd9d175
Yoni Belson <ybelson18@gmail.com>
2024-12-12 16:33:12 +0200

    fix: increase spacing between button and dashboard image

--- components/hero.tsx
+++ components/hero.tsx
@@ -66,9 +66,9 @@ export const Hero = () => {
     return isMobile ? [0.7, 0.9] : [1.05, 1.2];
   };
 
-  const rotate = useTransform(scrollYProgress, [0, 0.5], [20, 0]);
   const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions());
   const translate = useTransform(scrollYProgress, [0, 1], [0, 100]);
+
   return (
     <div
       ref={containerRef}
@@ -148,14 +148,11 @@ export const Hero = () => {
           </div>
         </div>
       </Container>
-      <div className="flex items-center justify-center relative p-2 md:p-20 cursor-pointer md:-mt-20 w-full">
+      <div className="flex items-center justify-center relative p-2 md:p-20 cursor-pointer md:-mt-20 w-full mt-40 md:mt-20">
         <div
           className="w-full relative overflow-x-hidden md:overflow-x-visible"
-          style={{
-            perspective: "1000px",
-          }}
         >
-          <Card rotate={rotate} translate={translate} scale={scale}>
+          <Card translate={translate} scale={scale}>
             <Image
               src={`/dashboard.png`}
               alt="hero"
@@ -173,12 +170,10 @@ export const Hero = () => {
 };
 
 export const Card = ({
-  rotate,
   scale,
   translate,
   children,
 }: {
-  rotate: MotionValue<number>;
   scale: MotionValue<number>;
   translate: MotionValue<number>;
   children: React.ReactNode;
@@ -186,7 +181,6 @@ export const Card = ({
   return (
     <motion.div
       style={{
-        rotateX: rotate,
         translateY: translate,
         boxShadow:
           "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
