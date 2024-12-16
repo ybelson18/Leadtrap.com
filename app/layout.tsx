13d8038f045cea8e4cd648dbb9527f8c23b9ee21
Yoni Belson <ybelson18@gmail.com>
2024-12-16 20:10:40 +0200

    feat: add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,6 +88,20 @@ export default function RootLayout({
             `
             }}
           />
+          {/* Google tag (gtag.js) */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-JW1QNDK36Q"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-JW1QNDK36Q');
+            `}
+          </Script>
         </head>
         <body
           className={cn(
